94c9c039893587ba02ce27ea33c47de939a7e7b9
liwy <535767595@qq.com>
2025-04-03 15:09:24 +0800

    补充更多方法

--- funk/funk_test.go
+++ funk/funk_test.go
@@ -36,3 +36,68 @@ func TestFunkMap(t *testing.T) {
 		})
 	})
 }
+
+// 对切片或者数组中的每一个元素应用一个新的函数，返回新的切片或者数据
+func TestFunkFilter(t *testing.T) {
+	Convey("Test funk.Filter", t, func() {
+		numbers := []int{1, 2, 3, 4}
+		Convey("Filter even numbers", func() {
+			So(funk.Filter(numbers, func(x int) bool { return x%2 == 0 }).([]int), ShouldResemble, []int{2, 4})
+		})
+	})
+}
+
+// 对切片或者数组中的每一个元素应用一个新的函数，返回新的切片或者数据
+func TestFunkReduce(t *testing.T) {
+	Convey("Test funk.Reduce", t, func() {
+		numbers := []int{1, 2, 3, 4}
+		Convey("Sum all elements", func() {
+			So(funk.Reduce(numbers, func(acc, x int) int { return acc + x }, 0), ShouldEqual, 10)
+		})
+	})
+}
+
+// 对切片或者数组中的每一个元素应用一个新的函数，返回新的切片或者数据
+func TestFunkForEach(t *testing.T) {
+	Convey("Test funk.ForEach", t, func() {
+		numbers := []int{1, 2, 3, 4}
+		Convey("Print each element", func() {
+			var result []int
+			funk.ForEach(numbers, func(x int) { result = append(result, x) })
+			So(result, ShouldResemble, []int{1, 2, 3, 4})
+		})
+	})
+}
+
+// funk.Uniq
+// 对切片或者数组中的每一个元素应用一个新的函数，返回新的切片或者数据
+func TestFunkUniq(t *testing.T) {
+	Convey("Test funk.Uniq", t, func() {
+		numbers := []int{1, 2, 2, 3, 4, 4}
+		Convey("Remove duplicates", func() {
+			So(funk.Uniq(numbers).([]int), ShouldResemble, []int{1, 2, 3, 4})
+		})
+	})
+}
+
+// funk.Keys
+// 对切片或者数组中的每一个元素应用一个新的函数，返回新的切片或者数据
+func TestFunkKeys(t *testing.T) {
+	Convey("Test funk.Keys", t, func() {
+		m := map[string]int{"a": 1, "b": 2, "c": 3}
+		Convey("Get keys", func() {
+			So(funk.Keys(m).([]string), ShouldResemble, []string{"a", "b", "c"})
+		})
+	})
+}
+
+// funk.Values
+// 对切片或者数组中的每一个元素应用一个新的函数，返回新的切片或者数据
+func TestFunkValues(t *testing.T) {
+	Convey("Test funk.Values", t, func() {
+		m := map[string]int{"a": 1, "b": 2, "c": 3}
+		Convey("Get values", func() {
+			So(funk.Values(m).([]int), ShouldResemble, []int{1, 2, 3})
+		})
+	})
+}
